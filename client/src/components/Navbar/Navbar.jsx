dbce93900cf8917091d1fa93d714758b78731774
Victor Ramirez <vicdeveloper83@gmail.com>
2022-11-12 13:02:44 +0000

    working on nav

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import styles from './Navbar.module.css';
 import logo from '../../assets/images/logo.png';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
       <div className={styles.logoContainer}>
         <img className={styles.logo} src={logo} alt='logo' />
       </div>
+      <ul>
+        <Link to=''>Home</Link>
+        <Link to=''>Add Videogame</Link>
+        <Link to=''>find Videogame</Link>
+        <Link to=''>edit Videogame</Link>
+      </ul>
     </nav>
   );
 };
