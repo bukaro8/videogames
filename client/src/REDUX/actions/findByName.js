dcb9f831be3df7e44e90c72fe479c0a7abd10adb
Victor Ramirez <vicdeveloper83@gmail.com>
2022-11-13 21:07:45 +0000

    loadapi addvideogames and findbyname  created

--- client/src/REDUX/actions/findByName.js
+++ client/src/REDUX/actions/findByName.js
@@ -4,7 +4,7 @@ import { FIND_BY_NAME } from '../action-types';
 export const findByName = (name) => {
   return async (dispatch, getState) => {
     try {
-      const response = await back.get('/videogames');
+      const response = await back.get(`/videogames?name=${name}`);
       dispatch({
         type: FIND_BY_NAME,
         payload: response.data,
--- client/src/REDUX/actions/index.js
+++ client/src/REDUX/actions/index.js
@@ -1,6 +1,9 @@
 import { loadApi } from './loadApi.js';
-
+import { add_videogame } from './addVideogame.js';
+import { findByName } from './findByName.js';
 const actions = {
   loadApi,
+  add_videogame,
+  findByName,
 };
 export default actions;
