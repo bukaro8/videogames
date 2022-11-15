db677b56e9da60abbaf2a9fe701dc420a5b423ea
Victor Ramirez <vicdeveloper83@gmail.com>
2022-11-15 16:43:18 +0000

    working on home component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,7 +1,12 @@
 import { React, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadApiGames } from '../../REDUX/actions.index.js';
 import style from './Home.module.css';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const allVideogames = useSelector((state) => state.allVideogames);
+  useEffect(() => {}, []);
   return (
     <div>
       <h1 className={style.title}>im the title</h1>
