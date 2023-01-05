9d9f094c0a637650b85870a7a5acf5f47e3cbc2b
Victor Ramirez <vicdeveloper83@gmail.com>
2023-01-05 01:08:04 +0000

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -10,19 +10,21 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-server.use(cors);
+
+server.use(express.json());
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-	res.setHeader(
-		'Access-Control-Allow-Methods',
-		'Content-Type',
-		'Authorization'
+	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
 	);
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 	next();
 });
 
