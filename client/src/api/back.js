58597056cf27ab2c4b8e48dc4f947c07844d2d80
Victor Ramirez <vicdeveloper83@gmail.com>
2023-01-05 00:26:08 +0000

    ultimo intento hoy

--- client/src/api/back.js
+++ client/src/api/back.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const back = axios.create({
-	baseURL: 'https://videogames-production-5367.up.railway.app',
+	baseURL: 'videogames-production-5367.up.railway.app/',
 });
 export default back;
